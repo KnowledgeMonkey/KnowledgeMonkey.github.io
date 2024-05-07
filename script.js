efb2f4c4118841e96e0ff4302c536bedd41c9e17
Knowledge Monkey <110980708+KnowledgeMonkey@users.noreply.github.com>
2024-05-07 21:45:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebas
 import { getDatabase, ref, push, update, onValue } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAW-ZeUlzcKMoQYOhryWKXaL5uybNea3Yk",
-    authDomain: "noxxed.firebaseapp.com",
-    databaseURL: "https://noxxed-default-rtdb.firebaseio.com",
-    projectId: "noxxed",
-    storageBucket: "noxxed.appspot.com",
-    messagingSenderId: "75167503010",
-    appId: "1:75167503010:web:9cef5d7e7f98165bfbfdfb",
-    measurementId: "G-36W00TY9GQ"
+  apiKey: "AIzaSyAW-ZeUlzcKMoQYOhryWKXaL5uybNea3Yk",
+  authDomain: "noxxed.firebaseapp.com",
+  databaseURL: "https://noxxed-default-rtdb.firebaseio.com",
+  projectId: "noxxed",
+  storageBucket: "noxxed.appspot.com",
+  messagingSenderId: "75167503010",
+  appId: "1:75167503010:web:9cef5d7e7f98165bfbfdfb",
+  measurementId: "G-36W00TY9GQ"
 };
 
 const app = initializeApp(firebaseConfig);
