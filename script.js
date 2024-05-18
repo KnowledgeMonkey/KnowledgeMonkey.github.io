76b29bc189d130a4f432efd73fe5d486358d3070
knowledgemonkey <schulmanager327@gmail.com>
2024-05-18 15:24:10 +0200

    Changed to Bot Website

--- script.js
+++ script.js
@@ -1,113 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-import { getDatabase, ref, push, update, onValue } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAW-ZeUlzcKMoQYOhryWKXaL5uybNea3Yk",
-  authDomain: "noxxed.firebaseapp.com",
-  databaseURL: "https://noxxed-default-rtdb.firebaseio.com",
-  projectId: "noxxed",
-  storageBucket: "noxxed.appspot.com",
-  messagingSenderId: "75167503010",
-  appId: "1:75167503010:web:9cef5d7e7f98165bfbfdfb",
-  measurementId: "G-36W00TY9GQ"
-};
-
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-document.addEventListener("DOMContentLoaded", function() {
-    const inputName = document.getElementById("inputField2");
-    const inputText = document.getElementById("inputField");
-
-    inputName.addEventListener("input", function() {
-        inputName.classList.remove("input-focused");
-    });
-
-    inputText.addEventListener("input", function() {
-        inputText.classList.remove("input-focused");
-    });
-
-    const button = document.getElementById("button");
-    const chatContainer = document.getElementById("chatContainer");
-    const maxMessages = 10; // Maximum number of displayed messages
-    const messagesQueue = [];
-
-    button.addEventListener("click", () => {
-        let inputNameVal = inputName.value.trim();
-        let inputTextVal = inputText.value.trim();
-
-        if (inputNameVal !== '' && inputTextVal !== '') {
-            if (/^[a-zA-Z]+$/.test(inputNameVal)) {
-                sendMessage(inputNameVal, inputTextVal);
-            } else {
-                alert("Please enter a valid name with only letters (A-Z, a-z).");
-            }
-        } else {
-            alert("Please enter your name and message.");
-        }
-    });
-
-    onValue(ref(database, 'chat'), (snapshot) => {
-        messagesQueue.length = 0; // Clear messages queue
-        snapshot.forEach((childSnapshot) => {
-            const message = childSnapshot.val();
-            const name = message.name;
-            const text = message.message;
-            const timestamp = message.timestamp;
-            messagesQueue.push({ name, text, timestamp });
-        });
-        displayMessages();
-    });
-
-    function displayMessages() {
-        chatContainer.innerHTML = '';
-        if (messagesQueue.length === 0) return; // Exit if there are no messages
-        const startIndex = Math.max(0, messagesQueue.length - maxMessages);
-        for (let i = startIndex; i < messagesQueue.length; i++) {
-            const message = messagesQueue[i];
-            displayMessage(message.name, message.text, message.timestamp);
-        }
-    }
-
-    function displayMessage(name, message, timestamp) {
-        const messageDiv = document.createElement('div');
-        messageDiv.classList.add('message');
-        const timestampString = new Date(timestamp).toLocaleTimeString();
-        messageDiv.innerHTML = `<div class="message-text"><strong>${name}:</strong> <span>${message}</span></div><div class="timestamp">${timestampString}</div>`;
-        chatContainer.appendChild(messageDiv);
-    }
-
-    function sendMessage(name, message) {
-        const messageRef = push(ref(database, 'chat'));
-        const messageKey = messageRef.key;
-
-        const newMessage = {
-            name: name,
-            message: message,
-            timestamp: Date.now()
-        };
-
-        const updates = {};
-        updates['/chat/' + messageKey] = newMessage;
-
-        update(ref(database), updates);
-
-        inputText.value = '';
-    }
+document.getElementById('confirm-age').addEventListener('click', function() {
+    document.getElementById('age-verification').style.display = 'none';
+    document.getElementById('content').classList.add('active');
 });
 
+document.getElementById('deny-age').addEventListener('click', function() {
+    window.location.href = 'https://www.google.com';
+});
 
-
-
-
-
-
-
-        const updates = {};
-        updates['/chat/' + messageKey] = newMessage;
-
-        update(ref(database), updates);
-
-        inputText.value = '';
-    
-
+document.getElementById('add-bot-btn').addEventListener('click', function() {
+    window.location.href = 'https://discord.com/oauth2/authorize?client_id=1201207793676468224&permissions=2048&response_type=code&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Foauth2%2Fauthorize%3F%26client_id%3D1201207793676468224%26scope%3Dbot%2Bapplications.commands%2Bmessages.read%2Bapplications.commands.permissions.update&scope=applications.commands.permissions.update+applications.commands+messages.read+bot';
+});
