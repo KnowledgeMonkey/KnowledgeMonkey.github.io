1a5000bf82ea820510b779938b1c3b2cab04ac6c
Knowledge Monkey <110980708+KnowledgeMonkey@users.noreply.github.com>
2024-08-14 13:16:23 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const elements = document.querySelectorAll('.container, .tutorial, footer');
+    const elements = document.querySelectorAll('.container, .guide-box, footer');
     elements.forEach(el => {
         el.style.opacity = 0;
         el.style.transition = 'opacity 1s ease-in-out';
